1302166fc255775b9f23f3f0e6b8e3fa57806d83
Antoine Giraud <antoine@lecoati.com>
2017-01-10 10:48:47 +0100

    Add dirty try just in case some DLL can be read

--- Src/Lecoati.uMirror/Properties/AssemblyInfo.cs
+++ Src/Lecoati.uMirror/Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.0.0.2107")]
-[assembly: AssemblyFileVersion("1.0.0.2107")]
+[assembly: AssemblyVersion("1.0.0.2108")]
+[assembly: AssemblyFileVersion("1.0.0.2108")]
